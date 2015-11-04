51aa79276050cfde5a314758e5b1b8feffad8d6d
ghosthenry2005 <saintsinner3@gmail.com>
2015-11-04 17:17:37 +0500

    Update 1fichier_com.php

--- hosts/1fichier_com.php
+++ hosts/1fichier_com.php
@@ -3,16 +3,17 @@
 class dl_1fichier_com extends Download {
 	
 	public function CheckAcc($cookie){
-		$data = $this->lib->curl("http://www.1fichier.com/en/console/abo.pl", $cookie, "");
-		if(stristr($data, "You are a premium user until")) return array(true, $this->lib->cut_str($data, "You are a premium user ","."));
+		$data = $this->lib->curl("https://1fichier.com/console/abo.pl", $cookie, "");
+		file_put_contents("test.txt",$data);
+		if(stristr($data, "Your account is already Premium.")) return array(true, $this->lib->cut_str($data, "Your subscription will end the ","."));
 		elseif(stristr($data, "You must be registered and logged in before subscribing")) return array(false, "accinvalid");
 			else return array(false, "accfree");
 	}
 	
 	public function Login($user, $pass){
-		$data = $this->lib->curl("https://www.1fichier.com/en/login.pl", "", "mail={$user}&pass={$pass}&lt=on&Login=Login");
+		$data = $this->lib->curl("https://1fichier.com/login.pl", "", "mail={$user}&pass={$pass}&lt=on&Login=Login");
 		$cookie = $this->lib->GetCookies($data);
-			return $cookie;
+		return $cookie;
 	}
 	
 	public function Leech($url) {
