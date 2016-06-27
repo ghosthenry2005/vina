89780d8153ab9bba25df5f197d79d2b85aee2b77
ghosthenry2005 <saintsinner3@gmail.com>
2016-06-27 21:18:17 +0500

    Update 1fichier_com.php

--- hosts/1fichier_com.php
+++ hosts/1fichier_com.php
@@ -4,7 +4,7 @@ class dl_1fichier_com extends Download {
 	
 	public function CheckAcc($cookie){
 		$data = $this->lib->curl("https://1fichier.com/console/abo.pl", $cookie, "");
-		if(stristr($data, "Your account is Premium until")) return array(true, $this->lib->cut_str($data, 'Your account is Premium until <span style="font-weight:bold">','</span>'));
+		if(stristr($data, "Your Premium offer subscription is valid")) return array(true, $this->lib->cut_str($data, 'Your Premium offer subscription is valid until <span style="font-weight:bold">','</span>'));
 		elseif(stristr($data, "You must be registered and logged in before subscribing")) return array(false, "accinvalid");
 			else return array(false, "accfree");
 	}
