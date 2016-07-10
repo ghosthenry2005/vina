7e284964292c13d91b5dfbf92416fba151d05536
ghosthenry2005 <saintsinner3@gmail.com>
2016-07-10 23:13:27 +0500

    Update 1fichier_com.php

--- hosts/1fichier_com.php
+++ hosts/1fichier_com.php
@@ -1,45 +1,34 @@
 <?php
 
 class dl_1fichier_com extends Download {
-
-	public function CheckAcc($cookie) {
+	
+	public function CheckAcc($cookie){
 		$data = $this->lib->curl("https://1fichier.com/console/abo.pl", $cookie, "");
-		if (stristr($data, 'Your account is Premium until')) return array(true, "Premium Until: ".$this->lib->cut_str($data, "until ","</span>"));
-		elseif (stristr($data, 'After test for FREE our services, choose your Offer')) return array(false, "accfree");
-		else return array(false, "accinvalid");
+		if(stristr($data, "Your Premium offer subscription is valid")) return array(true, $this->lib->cut_str($data, 'Your Premium offer subscription is valid until <span style="font-weight:bold">','</span>'));
+		elseif(stristr($data, "You must be registered and logged in before subscribing")) return array(false, "accinvalid");
+			else return array(false, "accfree");
 	}
 	
-	public function Login($user, $pass) {
-		$data = $this->lib->curl("https://1fichier.com/login.pl", "", "mail={$user}&pass={$pass}&lt=on&valider=Send");
-		$cookie = "LG=en; ".$this->lib->GetCookies($data);
+	public function Login($user, $pass){
+		$data = $this->lib->curl("https://1fichier.com/login.pl", "", "mail={$user}&pass={$pass}&lt=on&Login=Login");
+		$cookie = $this->lib->GetCookies($data);
 		return $cookie;
 	}
-
+	
 	public function Leech($url) {
-		list($url, $pass) = $this->linkpassword($url);
-		$url = str_replace('http://', 'https://', $url);
-		$data = $this->lib->curl($url, $this->lib->cookie, "pass={$pass}");
-		if (stristr($data, 'Password Protected')) $this->error("reportpass", true, false);
-		elseif (stristr($data, 'Incorrect Password')) $this->error("wrongpass", true, false, 2);
-		elseif (stristr($data, 'file could not be found') || stristr($data, 'file has been deleted')) $this->error("dead", true, false, 2);
-		elseif ($this->isredirect($data)) {
-			$link = str_replace('https', 'http', $this->redirect);
-			$filename = explode(';', $this->lib->getname($link, $this->lib->cookie));
-			$this->lib->reserved['filename'] = $filename[0];
-			return $link;
-		}
+		$data = $this->lib->curl($url, $this->lib->cookie, "did=0");
+		if(stristr($data, "The requested file could not be found")) $this->error("dead", true, false, 2);
+		elseif($this->isredirect($data)) return trim($this->redirect);
 		return false;
 	}
 
 }
 
 /*
- * Open Source Project
- * Vinaget by ..::[H]::..
- * Version: 2.7.0
- * 1fichier Download Plugin 
- * Downloader Class By [FZ]
- * Fixed Check Account by Enigma [06.02.2016]
- * [Jetleech.com]
- */
+* Open Source Project
+* Vinaget by ..::[H]::..
+* Version: 2.7.0
+* 1fichier Download Plugin 
+* Downloader Class By [FZ]
+*/
 ?>
